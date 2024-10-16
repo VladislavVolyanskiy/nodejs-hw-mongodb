5aaa726b912bf1165f52e96f1ec8eb154b709514
VladislavVolyanskiy <vladislavvolyanskiy@gmail.com>
2024-10-16 23:37:38 +0300

    add validation

---
+++ src/validation/contacts.js
@@ -0,0 +1,13 @@
+import Joi from "joi";
+
+export const createContactSchema = Joi.object({
+    name: Joi.string().min(3).max(20).required(),
+    email: Joi.string().email().required(),
+
+  });
+
+  export const updateContactSchema = Joi.object({
+    name: Joi.string().min(3).max(20),
+    email: Joi.string().email(),
+
+  });
