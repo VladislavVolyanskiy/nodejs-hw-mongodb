2b517f136dc2b5a8a64212eb0811dc384a52f610
VladislavVolyanskiy <vladislavvolyanskiy@gmail.com>
2024-10-17 23:06:50 +0300

    fix validation schema and routers

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -9,23 +9,38 @@ import {
 } from '../controllers/contacts.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import { validateBody } from '../middlewares/validateBody.js';
-import { createContactSchema, updateContactSchema } from '../validation/contacts.js';
+import {
+  createContactSchema,
+  updateContactSchema,
+} from '../validation/contacts.js';
 import { isValidId } from '../middlewares/isValidId.js';
 
 const router = Router();
 
 router.get('/contacts', ctrlWrapper(getAllContactsController));
 
-router.get('/contacts/:contactId', isValidId, ctrlWrapper(getContactByIdController));
+router.get(
+  '/contacts/:contactId',
+  isValidId,
+  ctrlWrapper(getContactByIdController),
+);
 
-router.post('/contacts', ctrlWrapper(createContactController));
+router.delete(
+  '/contacts/:contactId',
+  isValidId,
+  ctrlWrapper(deleteContactController),
+);
 
-router.patch('/contacts/:contactId', isValidId, ctrlWrapper(patchContactController));
+router.post(
+  '/contacts',
+  validateBody(createContactSchema),
+  ctrlWrapper(createContactController),
+);
 
-router.delete('/contacts/:contactId', isValidId, ctrlWrapper(deleteContactController));
-
-router.post('/', validateBody(createContactSchema), ctrlWrapper(createContactController),);
-
-router.patch('/contacts/:contactId', validateBody(updateContactSchema), ctrlWrapper(patchContactController),);
+router.patch(
+  '/contacts/:contactId',
+  validateBody(updateContactSchema),
+  ctrlWrapper(patchContactController),
+);
 
 export default router;
--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -1,13 +1,19 @@
-import Joi from "joi";
+import Joi from 'joi';
 
 export const createContactSchema = Joi.object({
-    name: Joi.string().min(3).max(20).required(),
-    email: Joi.string().email().required(),
+  name: Joi.string().min(3).max(20).required(),
+  phoneNumber: Joi.string().min(3).max(20).required(),
+  email: Joi.string().email().required(),
+  contactType: Joi.string()
+    .required()
+    .valid('work', 'home', 'personal')
+    .default('personal'),
+});
 
-  });
-
-  export const updateContactSchema = Joi.object({
-    name: Joi.string().min(3).max(20),
-    email: Joi.string().email(),
-
-  });
+export const updateContactSchema = Joi.object({
+  name: Joi.string().min(3).max(20),
+  phoneNumber: Joi.string().min(3).max(20),
+  email: Joi.string().email(),
+  isFavourite: Joi.boolean(),
+  contactType: Joi.string().valid('work', 'home', 'personal'),
+}).or('name', 'phone', 'email', 'isFavourite', 'contactType');
