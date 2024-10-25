9ce97fa060b53f94be9d18c3c10290b9b3e3d8e0
VladislavVolyanskiy <vladislavvolyanskiy@gmail.com>
2024-10-25 22:29:22 +0300

    fix routes

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -35,22 +35,10 @@ router.post('/refresh', ctrlWrapper(refreshUserSessionController));
 
 router.post('/logout', ctrlWrapper(logoutUserController));
 
-// router.post(
-//   '/request-reset-email',
-//   validateBody(requestResetEmailSchema),
-//   ctrlWrapper(requestResetEmailController),
-// );
-
 router.post(
   '/send-reset-email',
   validateBody(requestResetEmailSchema),
   ctrlWrapper(requestResetEmailController),
-);
-
-router.post(
-  '/reset-password',
-  validateBody(resetPasswordSchema),
-  ctrlWrapper(resetPasswordController),
 );
 
 router.post(
